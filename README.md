a7be41f6b36ed3f06a33aee0e9fe8745127ca692
Bruno Becoski <brunobecoski@gmail.com>
2021-01-04 23:21:39 -0300

    Rename README.mk to README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alura Javacript
+
+Esse é um projeto utilizando a API pública da Alura.
+
+https://suporte.alura.com.br/article/315-como-funciona-a-api-publica-da-alura
--- README.mk
+++
@@ -1,5 +0,0 @@
-# Alura Javacript
-
-Esse é um projeto utilizando a API pública da Alura.
-
-https://suporte.alura.com.br/article/315-como-funciona-a-api-publica-da-alura
