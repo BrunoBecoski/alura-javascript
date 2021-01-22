60bcf73526b178e7a0be4fd643bd2292315794aa
Bruno Becoski <brunobecoski@gmail.com>
2021-01-21 23:44:45 -0300

    Update page formation

--- scripts/formation_scripts.js
+++ scripts/formation_scripts.js
@@ -4,23 +4,35 @@ async function requestApi() {
   return await requestData('https://www.alura.com.br/api/formacoes');
 } 
 
-render();
+render() 
 
 async function render() {
 
-  const section = document.querySelector("#section-content");
-
   const dataResponse = await requestApi()
-  
-  dataResponse.body.forEach(data => {
+  const body = dataResponse.body;
+
+  body.map(course => {
+
+    const id = course.categoryUrlName;
+    const title = course.title;
+    const courseCount = course.courseCount;
+
+    const div = document.querySelector(`#${id}`);
+    const a = document.createElement("a");
+    const p = document.createElement("p");
+    const div2 = document.createElement("div");
+
     
-    if(document.querySelector(`#${data.categoryUrlName}`)) {
-      return;      
-    } else {
-      const h1 = document.createElement("h1");
-      h1.id = data.categoryUrlName;
-      h1.innerText = data.categoryName; 
-      section.appendChild(h1);
-    }
+
+    a.innerText = title;
+    a.href= "#";
+    p.innerText = `${courseCount} Cursos >`;
+
+    div2.appendChild(a);
+    div2.appendChild(p);
+
+    div.appendChild(div2);
+    
+  
   });
 }
