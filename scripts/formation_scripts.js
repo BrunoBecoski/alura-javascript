2cd60fdd7ac2bec544e620ee1d9d5ada08a9f5e3
Bruno Becoski <brunobecoski@gmail.com>
2021-01-22 23:44:21 -0300

    Update page formation

--- scripts/formation_scripts.js
+++ scripts/formation_scripts.js
@@ -11,25 +11,49 @@ async function render() {
   const dataResponse = await requestApi()
   const body = dataResponse.body;
 
+  // console.log(body);
+  
+  // let mobile = []; 
+  
+  // body.forEach(course => {
+
+  //   if(course.categoryName === 'Mobile') {
+  //     mobile.push({      
+  //       title: course.title,
+  //       count: course.courseCount
+  //     });
+  //   }
+  // })
+
+  // console.log(mobile);
+    
+
+  
   body.map(course => {
+    
 
     const id = course.categoryUrlName;
     const title = course.title;
     const courseCount = course.courseCount;
-
+    
     const div = document.querySelector(`#${id}`);
+    const p = document.querySelector(`#${id}-number`);
+
+
     const a = document.createElement("a");
-    const p = document.createElement("p");
+    const p2 = document.createElement("p");
     const div2 = document.createElement("div");
 
     
 
+    p.innerText = `X Formações`;
+
     a.innerText = title;
     a.href= "#";
-    p.innerText = `${courseCount} Cursos >`;
+    p2.innerHTML = `${courseCount} Cursos <span>></span>`;
 
     div2.appendChild(a);
-    div2.appendChild(p);
+    div2.appendChild(p2);
 
     div.appendChild(div2);
     
