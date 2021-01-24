fa8f0f8a73a36a362fa6e9deff0ef47459cafbbf
Bruno Becoski <brunobecoski@gmail.com>
2021-01-23 23:42:13 -0300

    Update page formation

--- scripts/formation_scripts.js
+++ scripts/formation_scripts.js
@@ -10,53 +10,133 @@ async function render() {
 
   const dataResponse = await requestApi()
   const body = dataResponse.body;
-
-  // console.log(body);
   
-  // let mobile = []; 
+  let devops = []; 
+  let mobile = []; 
+  let frontEnd = []; 
+  let designUx = []; 
+  let dataScience = []; 
+  let programacao = []; 
+  let inovacaoGestao = []; 
+  let marketingDigital = []; 
   
-  // body.forEach(course => {
+  body.forEach(course => {
 
-  //   if(course.categoryName === 'Mobile') {
-  //     mobile.push({      
-  //       title: course.title,
-  //       count: course.courseCount
-  //     });
-  //   }
-  // })
+    switch (course.categoryUrlName) {
 
-  // console.log(mobile);
-    
+      case 'mobile':
+        mobile.push({
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName     
+        });
+        break;
 
+      case 'design-ux':
+        designUx.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      case 'devops':
+        devops.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      case 'programacao':
+        programacao.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      case 'inovacao-gestao':
+        inovacaoGestao.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      case 'marketing-digital':
+        marketingDigital.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      case 'front-end':
+        frontEnd.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      case 'data-science':
+        dataScience.push({      
+          title: course.title,
+          count: course.courseCount,
+          id: course.categoryUrlName
+        });
+        break;
+
+      }
+  })
+
+  const allCourses = [ 
+    devops, 
+    mobile, 
+    frontEnd, 
+    designUx, 
+    dataScience, 
+    programacao, 
+    inovacaoGestao, 
+    marketingDigital
+  ];
   
-  body.map(course => {
-    
+  allCourses.map(course => {
 
-    const id = course.categoryUrlName;
-    const title = course.title;
-    const courseCount = course.courseCount;
-    
-    const div = document.querySelector(`#${id}`);
-    const p = document.querySelector(`#${id}-number`);
-
-
-    const a = document.createElement("a");
-    const p2 = document.createElement("p");
-    const div2 = document.createElement("div");
-
-    
-
-    p.innerText = `X Formações`;
-
-    a.innerText = title;
-    a.href= "#";
-    p2.innerHTML = `${courseCount} Cursos <span>></span>`;
-
-    div2.appendChild(a);
-    div2.appendChild(p2);
-
-    div.appendChild(div2);
-    
+    course.sort(function(a, b) {
+      var nameA = a.title.toUpperCase();
+      var nameB = b.title.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
   
+      return 0;
+    });
+
+    course.filter(data => {
+      
+      const divSelect = document.querySelector(`#${data.id}`);
+      const pSelect = document.querySelector(`#${data.id}-number`);
+  
+      const aCreate = document.createElement("a");
+      const pCreate = document.createElement("p");
+      const divCreate = document.createElement("div");
+  
+      pSelect.innerText = `${course.length} Formações`;
+  
+      aCreate.innerText = data.title;
+      aCreate.href= "#";
+      pCreate.innerHTML = `${data.count} Cursos <span>></span>`;
+  
+      divCreate.appendChild(aCreate);
+      divCreate.appendChild(pCreate);
+  
+      divSelect.appendChild(divCreate);
+    });  
   });
+
 }
